5ca8d9760aa1a30b0a06183b2755866a5c2bc486
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-03 12:40:31 +0200

    update

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,8 @@ class HelloController
      /**
      * @Route("/hello/{name?World}", name="hello")
      */
-    public function hello($name){
-        
+    public function hello($name, LoggerInterface $logger){
+        $logger->error("Mon message de log !");
         return new Response("Hello $name");
     }
 }
