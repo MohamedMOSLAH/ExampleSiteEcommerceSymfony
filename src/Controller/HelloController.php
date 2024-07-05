e0d80d92912c50695219029a4a8a131ba52b4d63
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-05 17:36:48 +0200

    update

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,11 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    
      /**
      * @Route("/hello/{name?World}", name="hello")
      */
-    public function hello($name, LoggerInterface $logger){
+    public function hello($name, LoggerInterface $logger,Calculator $calculator){
         $logger->error("Mon message de log !");
+        $tva = $calculator->calcul(100);
+        dump($tva);
         return new Response("Hello $name");
     }
 }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
+    protected $calculator;
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
     /**
      * @Route("/test", name="index")
      */
     public function index()
     {
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         dd("ça fonctionne");
     }
 
