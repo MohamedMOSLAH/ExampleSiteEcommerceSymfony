d97d0945964bba46491c5ac0645872016167d7d0
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-05 18:26:24 +0200

    update

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HelloController
 {
@@ -14,7 +16,11 @@ class HelloController
      /**
      * @Route("/hello/{name?World}", name="hello")
      */
-    public function hello($name, LoggerInterface $logger,Calculator $calculator){
+    public function hello($name, LoggerInterface $logger,Calculator $calculator, Slugify $slug,
+    Environment $twig
+    ){
+        dump($twig);
+        dump($slug->slugify("Hello world"));
         $logger->error("Mon message de log !");
         $tva = $calculator->calcul(100);
         dump($tva);
