60432733d055ff2f61a4adaedbdc67a7be4d2529
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-11 17:15:09 +0200

    insert product

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
  class HomeController extends AbstractController {
 
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(){
+    public function homepage(EntityManagerInterface $em){
 
+        $product = new Product;
+        $product->setName('Table en métal')
+                ->setPrice(3000)
+                ->setSlug('table-en-metal');
+        $em->persist($product);
+        $em->flush();
+            
+                
         return $this->render('home.html.twig');
     }
  }
