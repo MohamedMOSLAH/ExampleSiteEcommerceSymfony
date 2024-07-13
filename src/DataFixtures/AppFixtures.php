0279f13d1db9814da209b3d6d1fe982d876ec0a0
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-13 04:17:45 +0200

    add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($p = 0; $p < 100; $p++){
+            $product = new Product;
+            $product->setName("Produit n°$p")
+                    ->setPrice(mt_rand(100,200))
+                    ->setSlug("product-n-$p");
+            $manager->persist($product);        
+        }
+
+        $manager->flush();
+    }
+}
