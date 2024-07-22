ad52e5c26f50a8dc47911d9836dee880b7ecc821
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-07-22 16:36:35 +0200

    use DataTransformer

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -47,21 +48,37 @@ class ProductType extends AbstractType
                 ]);
             ;
     
-    
-         
-        $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
-            $product = $event->getData();
-
-            if($product->getPrice() !== null) {
-                $product->setPrice($product->getPrice() * 100);
+        $builder->get('price')->addModelTransformer(new CallbackTransformer(
+            function ($value){
+                if($value === null)  {
+                    return;
+                }
+                return $value / 100;
+            },
+            function ($value) {
+                if($value === null){
+                    return;
+                }
+                return $value * 100;
             }
-        });
+        ));
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+        //     $product = $event->getData();
 
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event){
-            $form = $event->getForm();
+        //     if($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
 
-            /** @var Product */
-            $product = $event->getData();
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event){
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
 
             // if($product->getId() === null){
             //     $form->add('category', EntityType::class, [
@@ -73,7 +90,7 @@ class ProductType extends AbstractType
             //         }
             //     ]);
             // }
-        });
+        //});
 
     }
 
