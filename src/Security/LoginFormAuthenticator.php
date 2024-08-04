e55e1231b52a547f0f3d153ddaaa22e5f7d6fdf2
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-08-04 17:50:43 +0200

    add failure function

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -6,45 +6,33 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
-class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+class LoginFormAuthenticator extends AbstractAuthenticator
 {
-    use TargetPathTrait;
-
-    public const LOGIN_ROUTE = 'security_login';
-
-    private UrlGeneratorInterface $urlGenerator;
-
     protected $encoder;
 
-
-
-    public function __construct(UserPasswordHasherInterface $encoder,UrlGeneratorInterface $urlGenerator)
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
-        $this->urlGenerator = $urlGenerator;
-        $this->encoder = $encoder;
+       $this->encoder = $encoder;
     }
 
-    public function supports(Request $request): bool
+    public function supports(Request $request): ?bool
     {
+        // TODO: Implement supports() method.
         return $request->attributes->get('_route') === 'security_login' && $request->isMethod('POST');
     }
 
-    public function authenticate(Request $request): Passport   
-    {       
-        $credentials = $request->request->all()['login'];        
+    public function authenticate(Request $request): Passport
+    {
+        // TODO: Implement authenticate() method.
+            $credentials = $request->request->all()['login'];        
         return new Passport( 
         new UserBadge($credentials['email']),           
         new PasswordCredentials($credentials['password'])    
@@ -54,11 +42,24 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-      dd("success");
+        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse('/'); 
     }
 
-    protected function getLoginUrl(Request $request): string
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+        return $request->attributes->get(Security::AUTHENTICATION_ERROR, $exception);
+        // TODO: Implement onAuthenticationFailure() method.
     }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
 }
