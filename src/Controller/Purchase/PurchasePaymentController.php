d84e3d04006110d90e8c19ee7b0dbb4baa606330
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-08-27 17:50:42 +0200

    update payment page

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Purchase;
 
+use App\Repository\PurchaseRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,7 +11,18 @@ class PurchasePaymentController extends AbstractController {
     /**
      * @Route("/purchase/pay/{id}", name="purchase_payment_form")
      */
-    public function showCardForm() {
-        return $this->render('purchase/payment.html.twig');
+    public function showCardForm($id, PurchaseRepository $purchaseRepository) {
+
+        $purchase = $purchaseRepository->find($id);
+        \Stripe\Stripe::setApiKey("sk_test_51PrS77HCnGw85cxmyi3MPluXYFsAlIVYVpYcJYwd4IZgiDS7dUc2Fa4yVeyKjO6va4BurAGFy77tVJFHkkmsrcNT00PEUPhiwV");
+        $intent = \Stripe\PaymentIntent::create([
+            'amount' => $purchase->getTotal(),
+            'currency' => 'eur'
+        ]);
+
+
+        return $this->render('purchase/payment.html.twig', [
+            'clientSecret' => $intent->client_secret
+        ]);
     }
 }
