1d182309c24953264d534af283ce996d79cf42b2
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-08-30 13:44:26 +0200

    create StripeService

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,24 +2,32 @@
 
 namespace App\Controller\Purchase;
 
+use App\Entity\Purchase;
 use App\Repository\PurchaseRepository;
+use App\Stripe\StripeService;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PurchasePaymentController extends AbstractController {
 
     /**
      * @Route("/purchase/pay/{id}", name="purchase_payment_form")
+     * @IsGranted("ROLE_USER")
      */
-    public function showCardForm($id, PurchaseRepository $purchaseRepository) {
+    public function showCardForm($id, PurchaseRepository $purchaseRepository, StripeService
+    $stripeService) {
 
         $purchase = $purchaseRepository->find($id);
-        \Stripe\Stripe::setApiKey("sk_test_51PrS77HCnGw85cxmyi3MPluXYFsAlIVYVpYcJYwd4IZgiDS7dUc2Fa4yVeyKjO6va4BurAGFy77tVJFHkkmsrcNT00PEUPhiwV");
-        $intent = \Stripe\PaymentIntent::create([
-            'amount' => $purchase->getTotal(),
-            'currency' => 'eur'
-        ]);
 
+        if(!$purchase || 
+            ($purchase && $purchase->getUser() !== $this->getUser()) || 
+            ($purchase && $purchase->getStatus() === Purchase::STATUS_PAID) )
+            {
+                return $this->redirectToRoute('cart_show');
+            }
+       
+        $intent = $stripeService->getPaymentIntent($purchase);
 
         return $this->render('purchase/payment.html.twig', [
             'clientSecret' => $intent->client_secret,
