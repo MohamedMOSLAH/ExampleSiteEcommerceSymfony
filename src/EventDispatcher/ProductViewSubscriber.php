a0ce65e718ea7894828d4888ac1d30d875314da4
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-09-05 16:51:09 +0200

    create ProductViewEvent

---
+++ src/EventDispatcher/ProductViewSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventDisptacher;
+
+use Psr\Log\LoggerInterface;
+use App\Event\ProductViewEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ProductViewEventSubscriber implements EventSubscriberInterface {
+
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'product.view' => 'sendEmail'
+        ];
+    }
+    public function sendEmail(ProductViewEvent $roductViewEvent) {
+       $this->logger->info("Email envoyé à l'admin pour le produit n°".$roductViewEvent->getProduct()->getId());
+    }
+
+}
